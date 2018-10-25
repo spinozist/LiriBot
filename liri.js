54b5d5491f6d0a17c477803d6a39e2154ecad271
Spinozist <ewoodworth@atlantaregional.org>
2018-10-25 18:34:50 -0400

    progress withe concert search

--- liri.js
+++ liri.js
@@ -31,23 +31,14 @@ var concertSearch = function () {
     console.log(`Searching Concerts for ${searchInput}`);
 
     request('https://rest.bandsintown.com/artists/nicki%20minaj/events?app_id=codingbootcamp', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        console.log('error:', error);
+        console.log('statusCode:', response && response.statusCode); 
+        // console.log('body:', JSON.parse(body,"/"));
+        var body = JSON.parse(body);
+        body.forEach (e => {
+            console.log(e.venue.name);
+        })
     });
-
-    //EXPRESS METHOD
-    // app.get(`https://rest.bandsintown.com/artists/nicki%20minaj/events?app_id=codingbootcamp`, function(response){
-    //     console.log(response);
-    // })
-
-    //JQUERY METHOD
-    // $.ajax({
-    //     url: `https://rest.bandsintown.com/artists/nicki%20minaj/events?app_id=codingbootcamp`,
-    //     method: `GET`
-    // }).then(function (response) {
-    //     console.log(response);
-    // })
 };
 
 var spotifySearch = function () {
